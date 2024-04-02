efa95a9c916ba72c95af8f0aa8ab854a0e3752ff
Pvasqz <117499420+Pvasqz@users.noreply.github.com>
2024-04-02 10:47:08 -0300

    act

--- peuler.py
+++ peuler.py
@@ -300,7 +300,7 @@ matrix = [
 
 invertida = [[row[i] for row in matrix] for i in range(3)]
 
-print(invertida)
+
 
 
 grid =   """08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
@@ -324,6 +324,8 @@ grid =   """08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
             20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
             01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48 """ 
 
+ss = np.array([[int(x) for x in grid.split()][i:i+20] for i in range(0,400,20)]).reshape(20, 20)
 
+print(ss)
 
 
